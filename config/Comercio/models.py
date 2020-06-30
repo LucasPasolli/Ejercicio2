0e153d5aaa859ca8e16d786a1d815f37529783e3
Lucas <lippasolli@hotmail.com>
2020-06-30 14:56:19 -0300

    Mas cosas en Model

--- config/Comercio/models.py
+++ config/Comercio/models.py
@@ -10,6 +10,12 @@ class Categoria(models.Model):
     def __str__(self):
         return '{}'.format(self.nombre)
 
+class Detalle(models.Model):
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.cantidad)
+
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=20)
@@ -19,14 +25,20 @@ class Producto(models.Model):
     def __str__(self):
         return '{}'.format(self.nombre)
 
-class Venta(models.Model):
-    id = models.AutoField(primary_key=True)
+class Venta(models.Model, Producto, Detalle):
+    id = models.AutoField(primary_key = True)
     fecha = models.DateField()
-    monto_final = models.FloatField()
     descuento = models.FloatField()
+    monto_final = models.FloatField()
+    Cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
+
+    def MontoFinal(self, Producto.precio, Detalle.cantidad):
+        precio = Producto.precio
+        cantidad = Detalle.cantidad
+        monto_final = precio * cantidad
 
     def __str__(self):
-        return '{}'.format(self.nombre)
+        return str(self.nombre)
 
 class Direccion(models.Model):
     calle = models.CharField(max_length=20)
@@ -35,14 +47,14 @@ class Direccion(models.Model):
     comuna = models.CharField(max_length=40)
 
     def __str__(self):
-        return '{}'.format(self.nombre)
+        return '{}'.format(self.calle)
 
 
 class Cliente(models.Model):
     rut = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=20)
     telefono = models.IntegerField()
-
+    direccion = models.ForeignKey(Direccion, on_delete = models.CASCADE)
     def __str__(self):
         return '{}'.format(self.nombre)
 
@@ -51,6 +63,7 @@ class Proveedor(models.Model):
     nombre = models.CharField(max_length=20)
     web = models.CharField(max_length=69)
     telefono = models.IntegerField()
+    direccion = models.ForeignKey(Direccion, on_delete = models.CASCADE)
 
     def __str__(self):
         return '{}'.format(self.nombre)
