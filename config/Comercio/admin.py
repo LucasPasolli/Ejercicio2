63d19cbb69fd70dadc3805a2c7732828210f101a
Lucas <lippasolli@hotmail.com>
2020-07-01 13:05:52 -0300

    Terminadas todas las Actividades

--- config/Comercio/admin.py
+++ config/Comercio/admin.py
@@ -6,24 +6,42 @@ class ClienteAdmin(admin.ModelAdmin):
     list_display = ['rut', 'nombre', 'telefono']
     list_display_links = ['rut', 'nombre', 'telefono']
     search_fields = ['rut', 'nombre', 'telefono']
+
+#class ProveedorInline(admin.StackedInline):
+    #model = Proveedor
+
 class ProductoAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
     fieldsets = (
-        ("Descripcion", {
-            'fields':('id','nombre',)
+        ('Descripcion', {
+            'fields':('categoria','nombre', 'proveedor',)
         }),
         ('Variables', {
             'fields':('precio','stock',)
         }),
     )
+    #inlines = [ProveedorInline]
 
+class ProductoInline(admin.TabularInline):
+    model = Producto
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('nombre','web',)
+    list_display_links = ('nombre',)
+    list_filter = ('nombre','rut',)
+    inlines = [ProductoInline,]
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('cliente','fecha', 'isDescuento')
+    list_display_links = ('fecha', 'cliente')
+    
 
 admin.site.register(Categoria)
 admin.site.register(Ciudad)
 admin.site.register(Comuna)
 admin.site.register(Direccion)
 admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(Venta)
-admin.site.register(Proveedor)
+admin.site.register(Venta, VentaAdmin)
+admin.site.register(Proveedor, ProveedorAdmin)
 admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Detalle)
--- config/Comercio/models.py
+++ config/Comercio/models.py
@@ -39,12 +39,20 @@ class Cliente(models.Model):
 class Venta(models.Model):
     id = models.AutoField(primary_key = True)
     fecha = models.DateField()
-    descuento = models.FloatField()
+    descuento = models.BooleanField()
     monto_final = models.FloatField()
-    Cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
     
+    def isDescuento(self):
+        return self.descuento
+    
+    isDescuento.boolean = True
+    isDescuento.short_descripcion = 'Descuento'
+
     def __str__(self):
-        return '{}'.format(self.id)
+        return str('Venta al Cliente: {}'.format(self.cliente.nombre))
+
+    
 
 class Proveedor(models.Model):
     rut = models.AutoField(primary_key=True)
@@ -61,6 +69,8 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=20)
     precio = models.FloatField()
     stock = models.IntegerField()
+    categoria = models.ForeignKey('Categoria',on_delete=models.CASCADE,null=True)
+    proveedor = models.ForeignKey('Proveedor',on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return '{}'.format(self.nombre)
